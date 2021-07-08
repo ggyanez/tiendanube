992c7498b9cf7613e66911b6ec54308c05bff598
Gabriel G. Yañez <gabo@MacBook-Pro-de-Gabriel.local>
2021-07-08 11:53:15 -0300

    Adding useful scripts

---
+++ addEventListenerOnFormSubmission.js
@@ -0,0 +1,35 @@
+(() => {
+    // This script should only run on step one of the checkout (/start/)
+    if (window.location.pathname.includes('/start/')) {
+
+        // Function that will be executed when the event is triggered (when the user submits the form and goes to step two of the checkout)
+        const myHandlerFunction = () => {
+            alert(`
+            Customer information submitted in the form:
+
+            Email: ${customerEmailInputElement ? customerEmailInputElement.value : ''}
+            -----
+            First Name (Shipping): ${customerShippingFirstNameInputElement ? customerShippingFirstNameInputElement.value : ''}
+            Last Name (Shipping): ${customerShippingLastNameInputElement ? customerShippingLastNameInputElement.value : ''}
+            Phone (Shipping): ${customerShippingPhoneInputElement ? customerShippingPhoneInputElement.value : ''}
+            -----
+            First Name (Billing): ${customerBillingFirstNameInputElement ? customerBillingFirstNameInputElement.value : ''}
+            Last Name (Billing): ${customerBillingLastNameInputElement ? customerBillingLastNameInputElement.value : ''}
+            Phone (Billing): ${customerBillingPhoneInputElement ? customerBillingPhoneInputElement.value : ''}      
+            `);
+        }
+
+        const stepOneForm = document.querySelector('form');
+        const customerEmailInputElement = document.querySelector("input[type='email']");
+        const customerShippingFirstNameInputElement = document.querySelector("input[name='shippingAddress.first_name']");
+        const customerShippingLastNameInputElement = document.querySelector("input[name='shippingAddress.last_name']");
+        const customerShippingPhoneInputElement = document.querySelector("input[name='shippingAddress.phone']");
+        const customerBillingFirstNameInputElement = document.querySelector("input[name='billingAddress.first_name']");
+        const customerBillingLastNameInputElement = document.querySelector("input[name='billingAddress.last_name']");
+        const customerBillingPhoneInputElement = document.querySelector("input[name='billingAddress.phone']");
+        
+        // Add the event listener to the form
+        stepOneForm.addEventListener('submit', myHandlerFunction);
+        return 'Custom event listener for form submission added'
+    }
+})();
---
+++ addEventListenerOnVariantChange.js
@@ -0,0 +1,21 @@
+(() => {
+    // This script should only run on product details page (i.e. /productos/campera-cuero/)
+    if (window.location.pathname.includes('/productos/') & window.location.pathname != 'productos') {
+
+        // Function that will be executed when the event is triggered (when the user selects a different variant option)
+        const myHandlerFunction = () => {
+            console.log('Variant changed');
+        }
+
+        // Get variant option HTML elements
+        const variantElements = document.querySelectorAll('.js-variation-option:not(.form-control-small)');
+
+        // Add the event listeners to those variant option elements
+        for (const variantElement in variantElements) {
+            if (typeof (variantElements[variantElement]) === 'object') {
+                variantElements[variantElement].addEventListener('change', myHandlerFunction);
+            }
+        }
+        return 'Custom event listeners for variant options added'
+    }
+})();
